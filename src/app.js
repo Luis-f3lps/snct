75321cfff438ded44ffcb1789e4e11d264adec65
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2026-05-21 17:30:16 -0300

    01

--- src/app.js
+++ src/app.js
@@ -25,19 +25,5 @@ app.get('/snct3', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'snct3.html'));
 });
 
-app.get('/resumos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'resumos_simples.html'));
-});
-
-app.get('/api/eventos', (req, res) => {
-    res.json([
-        { id: 1, nome: 'I Semana Nacional de Ciência e Tecnologia 2021', sigla: 'SNCT 1', link_imagem_fundo: '' },
-        { id: 2, nome: 'III Simpósio de Veterinária', sigla: 'Simpósio Vet', link_imagem_fundo: '' }
-    ]);
-});
-
-app.get('/api/resumos-simples', (req, res) => {
-    res.json(resumosDB);
-});
 
 module.exports = app;
