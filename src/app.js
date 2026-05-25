b4cf16e72fd0b81ea4258ec48192d581a7400401
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2026-05-25 16:44:07 -0300

    01

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.get('/', (req, res) => {
 app.get('/snct1', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'snct1.html'));
 });
-app.get('/snct11', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'snct11.html'));
+app.get('/snct4', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'snct4.html'));
 });
 app.get('/snct2', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'snct2.html'));
